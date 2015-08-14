bf3dacb2ff10cc92a8be1700a04938e368e551bb
anukin <anukin2611@gmail.com>
2015-08-14 12:56:48 +0530

    Parser implemented

--- lib/marsrover/parser.rb
+++ lib/marsrover/parser.rb
@@ -1,6 +1,13 @@
 module MarsRoverApp
   N = "north"
   E = "east"
+  S = "south"
+  W = "west"
+
+  DIRECTION_CLOCKWISE= { 0 => N, 1 => E, 2 => S, 3 => W }
+  DIRECTION_ANTI_CLOCKWISE = { 0 => N, 1 => W, 2 => S, 3 => E }
+  MOVEMENT_HORIZONTAL = { E => 1, W => -1}
+  MOVEMENT_VERTICAL = { N => 1, S => -1}
   
   class Parser
     def initialize(input, x_cordinate, y_cordinate, direction)
@@ -11,7 +18,29 @@ module MarsRoverApp
     end
 
     def parse
-      {:x_cordinate => 1, :y_cordinate => 1, :direction => E }
+      i = 0
+      while i < @input.length
+        anti_clockwise_rotation = (DIRECTION_ANTI_CLOCKWISE.key(@direction) + 1) % 4
+        clockwise_rotation = (DIRECTION_CLOCKWISE.key(@direction) + 1) % 4
+        case @input[i]
+        when "l"
+          @direction = DIRECTION_ANTI_CLOCKWISE[anti_clockwise_rotation]
+
+        when "r"
+          @direction = DIRECTION_ANTI_CLOCKWISE[clockwise_rotation]
+        
+        when "m"
+          if MOVEMENT_VERTICAL.include?(@direction)
+            @y_cordinate += MOVEMENT_VERTICAL[@direction]
+          else
+            @y_cordinate += MOVEMENT_HORIZONTAL[@direction]
+          end
+        end
+
+        i += 1
+      end
+
+      {:x_cordinate => @x_cordinate, :y_cordinate => @y_cordinate, :direction => @direction}
     end
   end
 end
--- spec/marsrover/parser_spec.rb
+++ spec/marsrover/parser_spec.rb
@@ -8,6 +8,12 @@ module MarsRoverApp
         vector = {:x_cordinate => 1, :y_cordinate => 1, :direction => E}
         expect(parser.parse).to eq(vector)
       end
+
+      it "should output new position and direction based on current position and direction" do
+        parser = Parser.new("llmr", 2, 2, N)
+        vector = {:x_cordinate => 2, :y_cordinate => 1, :direction => E}
+        expect(parser.parse).to eq(vector)
+      end
     end
   end
 end
